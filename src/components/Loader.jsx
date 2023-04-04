c846426923fc292cb3853a5551396920186537be
unknown <venugopalsp.64@gmail.com>
2023-04-04 16:19:48 +0530

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AnimatedLogo from '../assets/images/logo-animated.gif';
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+        <img src={AnimatedLogo} className='logo' alt="apple loader" />
+    </div>
+  )
+}
+
+export default Loader;
